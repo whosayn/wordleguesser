83ad589a8d3427d8182fe1b603eba9844ec67aa6
Hussein Faara <faarahussein@gmail.com>
2023-02-06 20:45:08 -0800

    Removing secret key from public
    Although the site is no longer live after heroku stopped hosting free
    apps, it's bad practice to have private keys exposed to the public.

--- wordleguesser/settings.py
+++ wordleguesser/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lv^&l25g4ak5g@j6)kicx@xu-lm9$3b09o*$3tdo(=np_w(x_i'
+SECRET_KEY = os.getenv("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
